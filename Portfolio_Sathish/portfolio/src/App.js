bb45a4bc2e3e6620915c1a4c639b62aa9184ac3d
Sathish <sathishbe970@gmail.com>
2022-06-06 12:28:12 +0530

    Project Setup

---
+++ Portfolio_Sathish/portfolio/src/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Portfolio_Sathish</h2>
+    </div>
+  );
+}
+
+export default App;
